fb78fb2826a976107b0896dbc9b916c82be9c000
Mauricio Monzon <104470486+Majomon@users.noreply.github.com>
2023-12-19 20:44:52 -0300

    Lavarropas

--- src/components/DetailInfoBot/DetailInfoBot.jsx
+++ src/components/DetailInfoBot/DetailInfoBot.jsx
@@ -23,7 +23,7 @@ function DetailInfoBot({ dataAxios,theme }) {
     kitchen: { value: "Cocina", img: Cocina },
     sewageDrain: { value: "Desague", img: Desague },
     balcony: { value: "Balcón", img: Balcon },
-    laundry: { value: "Lavadero", img: Lavadero },
+    laundry: { value: "Lavarropas", img: Lavadero },
     yard: { value: "Patio", img: Patio },
     dressingRoom: { value: "Vestidor", img: Vestidor },
   };
